657445950899a1defdbe78fec8dfd81487c51c9f
Clay McIlrath <clay.mcilrath@gmail.com>
2014-06-19 02:02:38 -0400

    Initial Commit

---
+++ Swift Pages/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Pages
+//
+//  Created by Clayton McIlrath on 6/19/14.
+//  Copyright (c) 2014 Unicorn Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
